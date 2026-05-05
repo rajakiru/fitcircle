6ad1e53a6a34136dadf30cdeedb9e4744d1f82d2
rajakiru <kiruthir@andrew.cmu.edu>
2026-05-05 20:04:05 +0900

    add app shell: layout, page, and global styles

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import FitCircleApp from '@/components/FitCircleApp';
+
+export default function Home() {
+  return (
+    <div className="app-shell">
+      <div className="phone-frame">
+        <FitCircleApp />
+      </div>
+    </div>
+  );
+}
